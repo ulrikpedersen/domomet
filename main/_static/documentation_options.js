58f416e8781fda66e6ffad5604c297eb7bff4f86
ulrikpedersen <ulrikpedersen@users.noreply.github.com>
2023-03-25 16:36:14 +0000

    deploy: 912e30805240e5c8ef1be1c462c5d7d29ea3ee4f

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2.dev4+gd12b881',
+    VERSION: '0.1.2.dev8+g912e308',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
