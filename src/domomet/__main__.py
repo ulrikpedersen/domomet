0441758265c8026cb1b894461e70aee57fb4635c
Ulrik Pedersen <ulrikkofoed@gmail.com>
2023-05-29 15:20:13 +0100

    Tweaking logging
    Enable setting loglevel from environment.
    Enable use of syslog/papertrail also configured through environment.

--- src/domomet/__main__.py
+++ src/domomet/__main__.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import socket
 from argparse import ArgumentParser
+from logging.handlers import SysLogHandler
 
 from dotenv import load_dotenv
 
@@ -10,11 +12,34 @@ __all__ = ["main"]
 
 
 def main(args=None):
+    log_level = os.getenv("DOMOMET_LOGLEVEL", default=logging.DEBUG)
+    if log_level not in logging.getLevelNamesMapping():
+        log_level = logging.DEBUG
     logging.basicConfig(
         level=logging.DEBUG,
         format="'%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)d - %(message)s'",  # noqa: E501
         datefmt="%d/%m/%Y %H:%M:%S",
     )
+
+    env_syslog_address_port = os.getenv("DOMOMET_SYSLOG_ADDR_PORT", default=None)
+    if env_syslog_address_port is not None:
+
+        class ContextFilter(logging.Filter):
+            hostname = socket.gethostname()
+
+            def filter(self, record):
+                record.hostname = ContextFilter.hostname
+                return True
+
+        syslog = SysLogHandler(address=env_syslog_address_port.split(":"))
+        syslog.addFilter(ContextFilter())
+        format = "%(asctime)s %(hostname)s DOMOMET: %(message)s"
+        formatter = logging.Formatter(format, datefmt="%b %d %H:%M:%S")
+        syslog.setFormatter(formatter)
+        logger = logging.getLogger()
+        logger.addHandler(syslog)
+        logger.setLevel(log_level)
+
     parser = ArgumentParser()
     parser.add_argument("--version", action="version", version=__version__)
     args = parser.parse_args(args)
